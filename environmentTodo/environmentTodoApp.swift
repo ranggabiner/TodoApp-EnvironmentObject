99948611b8c1b1f71540d92e13284443fda9058d
Rangga Biner <ranggabiner@gmail.com>
2024-11-13 09:22:05 +0700

    Initial Commit

---
+++ environmentTodo/environmentTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  environmentTodoApp.swift
+//  environmentTodo
+//
+//  Created by Rangga Biner on 13/11/24.
+//
+
+import SwiftUI
+
+@main
+struct environmentTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
